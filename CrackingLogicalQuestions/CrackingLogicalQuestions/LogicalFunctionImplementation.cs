42106dec6e5b4ba27ba66955bfe2ee017db6a72d
revanthkumar.makkena <revanthkumar.makkena@gmail.com>
2019-05-15 13:41:48 -0700

    Reversing a string using LINQ and Character Array

---
+++ CrackingLogicalQuestions/CrackingLogicalQuestions/LogicalFunctionImplementation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingLogicalQuestions
+{
+    class LogicalFunctionImplementation
+    {
+        public string ReverseString(string inputStr)
+        {
+            string outputStr = "";
+            
+            //Using LINQ Function 
+            IEnumerable<char> reversedChar = inputStr.Reverse();
+            foreach (var item in reversedChar)
+                outputStr += item;
+
+            //Using character Array 
+            char[] characterArrayOfGivenString = inputStr.ToCharArray();
+            for(int i =0, j=inputStr.Length-1; i<j; i++, j--)
+            {
+                characterArrayOfGivenString[i] = inputStr[j];
+                characterArrayOfGivenString[j] = inputStr[i];
+            }
+            string outputStrUsingCharArray = new string(characterArrayOfGivenString);
+
+            return $"LINQ Output - [{outputStr}]. CHAR ARRAY Output - [{outputStrUsingCharArray}]";
+        }
+    }
+}
---
+++ CrackingLogicalQuestions/CrackingLogicalQuestions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingLogicalQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogicalFunctionImplementation logics = new LogicalFunctionImplementation();
+            //Reverese a string
+            Console.WriteLine("Please Input a string");
+            Console.WriteLine(logics.ReverseString(Console.ReadLine()));
+
+            Console.ReadLine();
+        }
+       
+    }
+}
